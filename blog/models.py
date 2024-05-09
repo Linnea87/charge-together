0ced58e02b9837b9d2c0e34c129a9006da3a6de6
Linnea <linnea.ternevik@gmail.com>
2024-05-09 23:55:26 +0200

    migrate post model update post model and

--- blog/models.py
+++ blog/models.py
@@ -5,18 +5,18 @@ from django_resized import ResizedImageField
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
-class Posts(models.Model):
+class Post(models.Model):
     """
     A model to create and manage blog posts
     """
     title = models.CharField(max_length=200, unique=True)
     slug = AutoSlugField(populate_from='title', unique=True, always_update=True)
-    author = models.ForeignKey(User, related_name='posts_owner', on_delete=models.CASCADE)
-    excerpt = models.TextField()
+    author = models.ForeignKey(User, related_name='post_owner', on_delete=models.CASCADE)
     image = ResizedImageField(
         size =[400, None], quality=75, upload_to='blog/', force_format='WEBP', blank=False, null=False
     )
     image_alt = models.CharField(max_length=100, null=False, blank=False)
+    excerpt = models.TextField()
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
