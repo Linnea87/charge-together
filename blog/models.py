b280c3048da6e32041651eda16a235ab8ad59372
Linnea <linnea.ternevik@gmail.com>
2024-05-09 22:00:05 +0200

    add post model and new requirements

--- blog/models.py
+++ blog/models.py
@@ -1,2 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import User
+from autoslug import AutoSlugField
+from django_resized import ResizedImageField
 
+STATUS = ((0, "Draft"), (1, "Published"))
+
+class Posts(models.Model):
+    """
+    A model to create and manage blog posts
+    """
+    title = models.CharField(max_length=200, unique=True)
+    slug = AutoSlugField(populate_from='title', unique=True, always_update=True)
+    author = models.ForeignKey(User, related_name='posts_owner', on_delete=models.CASCADE)
+    excerpt = models.TextField()
+    image = ResizedImageField(
+        size =[400, None], quality=75, upload_to='blog/', force_format='WEBP', blank=False, null=False
+    )
+    image_alt = models.CharField(max_length=100, null=False, blank=False)
+    content = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+    updated_on = models.DateTimeField(auto_now=True)
+    
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return str(self.title)
