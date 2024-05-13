333ec182d84f16a2242438223b52f9243a7ab2eb
Linnea <linnea.ternevik@gmail.com>
2024-05-13 11:20:00 +0200

    Changed my staticfiles storage

--- blog/models.py
+++ blog/models.py
@@ -1,7 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
-from autoslug import AutoSlugField
 from django_resized import ResizedImageField
+from autoslug import AutoSlugField
+
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
