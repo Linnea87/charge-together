e846cbbcce4d58b760644876398bec2dbcd51f72
Linnea <linnea.ternevik@gmail.com>
2024-05-13 09:30:00 +0200

    update my  paths and  styling

--- chargetogether/urls.py
+++ chargetogether/urls.py
@@ -22,6 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('summernote/', include('django_summernote.urls')),
-    path('', include('home.urls')),
-    path('blog/', include('blog.urls'))
+    path('', include('home.urls'),),
+    path('blog/', include('blog.urls')),
 ]
