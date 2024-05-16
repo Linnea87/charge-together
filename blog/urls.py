fdc185cd1476a6d49d4a9eba4852d68d6dbe5946
Linnea Ternevik <linnea.ternevik@gmail.com>
2024-05-16 18:00:00 +0000

    add CRED to blog app

--- blog/urls.py
+++ blog/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import CreatePost, PostList, PostDetail
+from .views import (
+    CreatePost, PostList, 
+    PostDetail, DeletePost,
+    EditPost
+)
 
 urlpatterns = [
     path("create/", CreatePost.as_view(), name="create_post"),
-    path("post/", PostList.as_view(), name="post_list"),
+    path("", PostList.as_view(), name="post_list"),
     path("<slug:pk>/", PostDetail.as_view(), name="post_detail"),
+    path("delete/<slug:pk>/", DeletePost.as_view(), name="delete_post"),
+    path("edit/<slug:pk>/", EditPost.as_view(), name="edit_post"),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,14 @@
-from django.views.generic import CreateView, ListView, DetailView
+from django.views.generic import (
+    CreateView, ListView, 
+    DetailView, DeleteView,
+    UpdateView
+
+)
+
+from django.contrib.auth.mixins import (
+    UserPassesTestMixin, LoginRequiredMixin
+)
+
 from django.contrib.auth.mixins import LoginRequiredMixin
 from .models import Post
 from .forms import PostForm
@@ -8,7 +18,6 @@ class PostList(ListView):
     """
     view all blog posts
     """
-
     model = Post
     template_name = "blog/blog.html"
     queryset = Post.objects.filter(status=1).order_by("-created_on")
@@ -18,7 +27,6 @@ class PostDetail(DetailView):
     """
     Click and open a singel blog post
     """
-
     template_name = "blog/post_detail.html"
     model = Post
     queryset = Post.objects.filter(status=1).order_by("-created_on")
@@ -28,12 +36,34 @@ class CreatePost(LoginRequiredMixin, CreateView):
     """
     Add Post view
     """
-
     model = Post
     template_name = "blog/create_post.html"
     form_class = PostForm
     success_url = "/blog/"
 
     def form_valid(self, form):
-        form.instance.user = self.request.user
+        form.instance.user = self.request.author
         return super(CreatePost, self).form_valid(form)
+    
+
+class EditPost(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+    """
+    The User can edit their own existing blog post
+    """
+    template_name = "blog/edit_post.html"
+    model = Post
+    success_url = "/blog/"
+    form_class = PostForm
+    
+    def test_func(self):
+        return self.request.user == self.get_object().author
+
+class DeletePost(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
+    """
+    The user can delete their own existing blog post
+    """
+    model = Post
+    success_url = "/blog/"
+    
+    def test_func(self):
+        return self.request.user == self.get_object().author
