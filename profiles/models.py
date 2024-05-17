2dbf2e84c088aff4cabb9d418e85a55a5a308609
Linnea Ternevik <linnea.ternevik@gmail.com>
2024-05-17 16:27:21 +0000

    Add content to profile.html and profile urls , install and add RichTextField

--- profiles/models.py
+++ profiles/models.py
@@ -1,8 +1,8 @@
 from django.db import models
 from django.contrib.auth.models import User
+from djrichtextfield.models import RichTextField
 from django.db.models.signals import post_save
 from django.dispatch import receiver
-from django_summernote.fields import SummernoteTextField
 
 from django_resized import ResizedImageField
 
@@ -21,7 +21,7 @@ class Profile(models.Model):
         force_format="WEBP",
         blank=False,
     )
-    bio = SummernoteTextField(max_length=2500, null=True, blank=True)
+    bio = RichTextField(max_length=2500, null=True, blank=True)
 
     def __str__(self):
         return str(self.user.username)
